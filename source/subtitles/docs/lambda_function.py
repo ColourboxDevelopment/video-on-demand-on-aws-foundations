7e689c883b0a08bfda1b09bb3e35f25888d96d5c
Claus <claus@colourbox.com>
2022-06-15 15:13:47 +0200

    im an idiot..wrong file..

--- source/subtitles/docs/lambda_function.py
+++ source/subtitles/docs/lambda_function.py
@@ -10,21 +10,30 @@ import yaml
 
 from libraries.api import API
 
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
 def get_api_credentials():
     ssm_client = boto3.client('ssm')
     key = ssm_client.get_parameter(
             Name='/config/api/root/key', WithDecryption=True)['Parameter']['Value']
+    logger.info("FOUND KEY")
     secret = ssm_client.get_parameter(
             Name='/config/api/root/secret', WithDecryption=True)['Parameter']['Value']
+    logger.info("FOUND SECRET")
     return (key, secret)
 
 def lambda_handler(event, context):
+    logger.info("ARE WE EVEN HERE?!")
     docs_url = os.environ.get('API_URL')
     key, secret = get_api_credentials()
     api = API(key, secret, base_url = docs_url)
+    logger.info("initialized client")
     auth_str = api.get_authorization_header(key, secret)
     headers = {'Authorization': auth_str}
+    logger.info("ABOUT TO SEND REQUEST")
     r = requests.get(docs_url)
+    logger.info("REQUEST RETURNED")
     r.raise_for_status()
     return {
             "statusCode": 200,
--- source/subtitles/private-docs/lambda_function.py
+++ source/subtitles/private-docs/lambda_function.py
@@ -12,29 +12,20 @@ from libraries.api import API
 
 docs_url = "http://api.colourbox.com/docs-internal.yml"
 
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-
 def get_api_credentials():
     ssm_client = boto3.client('ssm')
     key = ssm_client.get_parameter(
             Name='/config/api/root/key', WithDecryption=True)['Parameter']['Value']
-    logger.info("FOUND KEY")
     secret = ssm_client.get_parameter(
             Name='/config/api/root/secret', WithDecryption=True)['Parameter']['Value']
-    logger.info("FOUND SECRET")
     return (key, secret)
 
 def lambda_handler(event, context):
-    logger.info("ARE WE EVEN HERE?!")
     key, secret = get_api_credentials()
     api = API(key, secret, base_url = docs_url)
-    logger.info("initialized client")
     auth_str = api.get_authorization_header(key, secret)
     headers = {'Authorization': auth_str}
-    logger.info("ABOUT TO SEND REQUEST")
     r = requests.get(docs_url, headers=headers)
-    logger.info("REQUEST RETURNED")
     r.raise_for_status()
     return {
             "statusCode": 200,
